7be348d173b4f515a088bb6e84cc14005fde3206
corey clarke <sombas100@yahoo.com>
2025-08-20 01:34:35 +0100

    Changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,7 +42,7 @@ export const PROJECTS = [
     image: project5,
     description:
       "Portora is a fullstack SaaS client portal designed to streamline project collaboration between freelancers and their clients. It offers a secure, centralized workspace for managing projects, exchanging files, sharing feedback, and monitoring progress all from a clean, intuitive dashboard.",
-    technologies: ["React", "TypeScript", "JavaScript", "Context API", "TailWindCSS", "NodeJS", "Socket.Io", "ExpressJS", "PostgreSQL", "Stripe" ],
+    technologies: ["React", "TypeScript", "JavaScript", "ContextAPI", "TailWindCSS", "NodeJS", "Socket.Io", "ExpressJS", "PostgreSQL", "Stripe" ],
     url: "https://www.portora.net/welcome",
   },
   {
